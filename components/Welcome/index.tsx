313d644beb930a6df111804c00c07ee36ad8a41c
Arnaud Rochez <a.rochez@viasatconnect.be>
2022-04-24 12:00:51 +0200

    [FIX] fixed missing onclick

--- components/Welcome/index.tsx
+++ components/Welcome/index.tsx
@@ -1,13 +1,32 @@
 import { useTranslation } from 'next-i18next';
 
 import Ids from '../../constants/ids';
+import { useResizeListener } from '../../hooks';
 import Button from '../atoms/Button';
 import { Body1, Heading1 } from '../atoms/typography';
+import { HEADER_SIZE, HEADER_SIZE_SMALL } from '../Header/style';
 import Leaves from './Leaves';
 import { WelcomeContainer, WelcomeWrapper } from './style';
 
 const Welcome = () => {
     const { t } = useTranslation('welcome');
+
+    const onDiscoverClick = () => {
+        const { width, height } = useResizeListener();
+        const element = document.getElementById(Ids.SERVICES);
+        if (!element) return;
+
+        const scrollOffset = width >= 600 ? HEADER_SIZE_SMALL : HEADER_SIZE;
+        const bodyRect = document.body.getBoundingClientRect().top;
+        const elementRect = element.getBoundingClientRect().top;
+        const elementPosition = elementRect - bodyRect;
+        const offsetPosition = elementPosition - scrollOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+        });
+    };
 
     return (
         <WelcomeContainer id={Ids.WELCOME}>
@@ -17,7 +36,9 @@ const Welcome = () => {
                     <br /> Green Views
                 </Heading1>
                 <Body1>{t('welcome-subtitle')}</Body1>
-                <Button color="secondary">{t('discover')}</Button>
+                <Button color="secondary" onClick={onDiscoverClick}>
+                    {t('discover')}
+                </Button>
                 <Leaves />
             </WelcomeWrapper>
         </WelcomeContainer>
